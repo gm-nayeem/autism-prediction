5018146bac06aed393d6c8c9c8fc0bca211a76b8
golam mostafa <gmnayeem25@gmail.com>
2024-02-26 01:42:56 +0600

    autism prediction project done

---
+++ src/App.jsx
@@ -0,0 +1,89 @@
+import React, { useState } from 'react';
+import Form from './components/Form';
+import { scoresData } from './utils/data';
+import { IoCheckbox } from "react-icons/io5";
+import { FaTriangleExclamation } from "react-icons/fa6";
+
+
+const App = () => {
+  const [info, setInfo] = useState([]);
+  const [isAutistic, setIsAutistic] = useState(false);
+  const [isSubmitted, setIsSubmitted] = useState(false);
+
+  const handleChange = (e) => {
+    const value = parseInt(e.target.value, 10);
+
+    setInfo(prev => [...prev, value]);
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const sum = info.reduce((sum, val) => sum += val, 0);
+
+    sum > 8 && setIsAutistic(true);
+
+    setIsSubmitted(true);
+  }
+
+
+  return (
+    <div className='w-full h-[100dvh] bg-slate-200 flex flex-col gap-4 justify-center items-center'>
+      <h1 className='font-bold text-2xl'>ASD Detection using Machine Learning</h1>
+      <div className='w-4/5 h-4/5 border-2 border-gray-500 p-6 flex flex-col gap-6'>
+        <div className='flex flex-wrap gap-4'>
+          {
+            scoresData?.length > 0 ? (
+              scoresData.map((data) => (
+                data?.id < 16 ? (
+                  <Form
+                    key={data.id}
+                    label={data.label}
+                    values={data.values}
+                    handleChange={handleChange}
+                  />
+                ) : (
+                  <Form
+                    key={data.id}
+                    label={data.label}
+                    values={data.values}
+                  />
+                )
+              ))
+            ) : null
+          }
+        </div>
+        <button
+          type="button"
+          onClick={handleSubmit}
+          className="w-max focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-md px-6 py-3 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
+        >
+          Autism Scanning
+        </button>
+
+        {
+          isSubmitted && (
+            <div className='flex flex-col gap-2'>
+              <h2 className='font-semibold text-lg'>{`The person has ${!isAutistic ? 'no' : ''} ASD`} <span className='text-green-700 font-bold'>95.89%</span></h2>
+              {
+                isAutistic ? (
+                  <div className='w-full flex gap-1 justify-left items-center bg-yellow-200 py-3 px-3 rounded-md'>
+                    <FaTriangleExclamation className='text-yellow-700' size={20} />
+                    <span className='text-sm text-gray-700'>The person has ASD</span>
+                  </div>
+                ) : (
+                  <div className='w-full flex gap-1 justify-left items-center bg-emerald-200 py-3 px-3 rounded-md'>
+                    <IoCheckbox className='text-green-700' size={20} />
+                    <span className='text-sm text-gray-700'>The person has no ASD</span>
+                  </div>
+                )
+              }
+            </div>
+          )
+        }
+      </div>
+    </div>
+  )
+}
+
+export default App;
---
+++ src/utils/data.js
@@ -0,0 +1,72 @@
+export const scoresData = [
+    { id: 1, label: 'A1_Score', name: 'A1_Score', values: [0, 1] },
+    { id: 2, label: 'A2_Score', name: 'A2_Score', values: [0, 1] },
+    { id: 3, label: 'A3_Score', name: 'A3_Score', values: [0, 1] },
+    { id: 4, label: 'A4_Score', name: 'A4_Score', values: [0, 1] },
+    { id: 5, label: 'A5_Score', name: 'A5_Score', values: [0, 1] },
+    { id: 6, label: 'A6_Score', name: 'A6_Score', values: [0, 1] },
+    { id: 7, label: 'A7_Score', name: 'A7_Score', values: [0, 1] },
+    { id: 8, label: 'A8_Score', name: 'A8_Score', values: [0, 1] },
+    { id: 9, label: 'A9_Score', name: 'A9_Score', values: [0, 1] },
+    {
+        id: 10,
+        label: 'A10_Score',
+        name: 'A10_Score',
+        values: [0, 1]
+    },
+    {
+        id: 11,
+        label: 'A11_Score',
+        name: 'A11_Score',
+        values: [0, 1]
+    },
+    {
+        id: 12,
+        label: 'A12_Score',
+        name: 'A12_Score',
+        values: [0, 1]
+    },
+    {
+        id: 13,
+        label: 'A13_Score',
+        name: 'A13_Score',
+        values: [0, 1]
+    },
+    {
+        id: 14,
+        label: 'A14_Score',
+        name: 'A14_Score',
+        values: [0, 1]
+    },
+    {
+        id: 15,
+        label: 'A15_Score',
+        name: 'A15_Score',
+        values: [0, 1]
+    },
+    {
+        id: 16,
+        label: 'Age',
+        name: 'Age',
+        values: [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+    },
+    {
+        id: 17,
+        label: 'Gender',
+        name: 'Gender',
+        values: ["Female", "Male"]
+    },
+    {
+        id: 18,
+        label: 'Who completed the test?',
+        name: 'gurdian',
+        values: ["Parents", "Relative", "Blooming Buds", "Spectrum Walfare Foundation", "DACAC", "GHAC", "Angel Foundation", "Theraphy Point", "Golden Kids Autism", "Dhamrai Buddhi Protibondi Biddalay"]
+    },
+    {
+        id: 19,
+        label: 'Class/ASD',
+        name: 'class',
+        values: ["YES", "NO"]
+    },
+];
+
